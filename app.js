b963bca1434c3abd10ffebd2a97544aa2dc17292
lalukrish <lalukrishnavallil01@gmail.com>
2024-07-30 20:16:54 +0530

    feat:user schema

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//mongodb
+mongoose
+  .connect(
+    "mongodb+srv://lalkirshna00:lalkrishna00@cluster0.1m34c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 app.use("/res", (req, res, next) => {
   res.send("hi");
   console.log("hi");
---
+++ models/userSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  pincode: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+});
+const User = mongoose.model("User", userSchema);
+module.exports = User;
